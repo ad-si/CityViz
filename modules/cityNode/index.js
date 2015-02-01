7efb5c1d710989a4d8c29e74f994e61b2a9fb0a9
adius <mail@adriansieber.com>
2015-02-01 22:18:55 +0100

    Modularize cityNode

--- modules/cityNode/index.js
+++ modules/cityNode/index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 require('es6-promise').polyfill()
 
 var fs = require('fs'),
@@ -7,65 +9,18 @@ var fs = require('fs'),
 	temp = require('temp'),
 	childProcess = require('child_process'),
 
-	parser = require('xml2json')
+	parser = require('xml2json'),
+
+	applyDefaults = require('../applyDefaults'),
+
+	javaPath = process.env.JAVA_HOME ?
+	           process.env.JAVA_HOME + '/bin/java' :
+	           'java'
 
 
-// TODO: Return promise when no callback is given
+function executeShellCommand (shellCommand, options, exportFile, actualExportFile, callback) {
 
-module.exports.export = function (midiBuffer, options, callback) {
-	// TODO
-}
-
-module.exports.exportSync = function (midiBuffer, options) {
-	// TODO
-	return
-}
-
-
-module.exports.getFromDb = function (options, callback) {
-
-	var defaults = {
-			config: {}, // Config file containing project settings
-			import: [], // Array of directories and files to import
-			format: 'xml' // xml, kml, kmz
-		},
-		actualExportFile,
-		shellCommand,
-		configFile,
-		exportFile,
-		javaPath,
-		key
-
-
-	// Set defaults
-	options = options || {}
-
-	for (key in defaults)
-		if (defaults.hasOwnProperty(key))
-			options[key] = options[key] || defaults[key]
-
-
-	if (options.id)
-		options.config
-			.project
-			.kmlExport
-			.filter
-			.simple
-			.gmlIds
-			.gmlId = {$t: options.id}
-
-	else if (options.ids)
-		options.config
-			.project
-			.kmlExport
-			.filter
-			.simple
-			.gmlIds
-			.gmlId = options.ids.map(function (id) {
-			return {$t: id}
-		})
-
-	configFile = temp.path()
+	var configFile = temp.path()
 
 	fs.writeFileSync(
 		configFile,
@@ -73,40 +28,26 @@ module.exports.getFromDb = function (options, callback) {
 		parser.toXml(options.config)
 	)
 
-	exportFile = path.resolve(__dirname, '..', options.outputFile) || temp.path()
-	actualExportFile = path.join(
-		path.dirname(exportFile),
-		path.basename(exportFile, path.extname(exportFile)) +
-		'_collada.' + options.format
-	)
-
-	javaPath = process.env.JAVA_HOME ?
-	           process.env.JAVA_HOME + '/bin/java' :
-	           'java'
-
-
-	shellCommand = [
-		javaPath,
-		'-jar',
-		'-Xms128m',
-		'-Xmx768m',
-		'lib/3dcitydb-impexp.jar',
-		'-shell',
-		(options.format === 'xml') ? '-export' : '-kmlExport',
-		exportFile,
-		'-config ' + configFile
-	].join(' ')
-
 	process.chdir('/Applications/3DCityDB-Importer-Exporter')
 
-	console.log(shellCommand)
+	shellCommand = shellCommand + ' -config ' + configFile
+
+	//console.log(shellCommand)
 
 	childProcess.exec(
 		shellCommand,
 		function (error, stdout, stderr) {
 
-			console.log(stdout.toString())
-			console.error(stderr.toString())
+			var stdoutText = stdout.toString(),
+				stderrText = stderr.toString()
+
+			console.log(stdoutText)
+
+			if (stderrText)
+				return callback(new Error(stderrText))
+
+			if (stdoutText.search(/error/gi) > 0)
+				return callback(new Error(stdoutText))
 
 			if (error)
 				return callback(error)
@@ -140,6 +81,97 @@ module.exports.getFromDb = function (options, callback) {
 					}, 0)
 				})
 		}
+	)
+}
+
+// TODO: Return promise when no callback is given
+
+module.exports.export = function (midiBuffer, options, callback) {
+	// TODO
+}
+
+module.exports.exportSync = function (midiBuffer, options) {
+	// TODO
+	return
+}
+
+
+module.exports.import = function (options, callback) {
+	var defaults = {
+		config: {}, // Config file containing project settings
+		import: [], // Array of directories and files to import
+	}
+
+	if (options.id)
+		options.config
+			.project
+			.kmlExport
+			.filter
+			.simple
+			.gmlIds
+			.gmlId = {$t: options.id}
+}
+
+
+module.exports.getFromDb = function (options, callback) {
+
+	var defaults = {
+			config: {}, // Config file containing project settings
+			import: [], // Array of directories and files to import
+			format: 'xml' // xml, kml, kmz
+		},
+		actualExportFile,
+		shellCommand,
+		exportFile
+
+
+	applyDefaults(defaults, options)
+
+	if (options.id)
+		options.config
+			.project
+			.kmlExport
+			.filter
+			.simple
+			.gmlIds
+			.gmlId = {$t: options.id}
+
+	else if (options.ids)
+		options.config
+			.project
+			.kmlExport
+			.filter
+			.simple
+			.gmlIds
+			.gmlId = options.ids.map(function (id) {
+			return {$t: id}
+		})
+
+	exportFile = path.resolve(__dirname, '..', options.outputFile) || temp.path()
+	actualExportFile = path.join(
+		path.dirname(exportFile),
+		path.basename(exportFile, path.extname(exportFile)) +
+		'_collada.' + options.format
+	)
+
+
+	shellCommand = [
+		javaPath,
+		'-jar',
+		'-Xms128m',
+		'-Xmx768m',
+		'lib/3dcitydb-impexp.jar',
+		'-shell',
+		(options.format === 'xml') ? '-export' : '-kmlExport',
+		exportFile
+	].join(' ')
+
+	executeShellCommand(
+		shellCommand,
+		options,
+		exportFile,
+		actualExportFile,
+		callback
 	)
 }
 
