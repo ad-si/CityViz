548cd4aa91731e8a6b038974e6c1ab630cd54132
adius <mail@adriansieber.com>
2015-02-01 22:18:04 +0100

    Add import feature to cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,13 +1,18 @@
 #! /usr/bin/env node
 
 var program = require('commander'),
-	packageData = require('../package.json')
+	packageData = require('../package.json'),
+	cityNode = require('modules/cityNode')
 
 
 program
 	.version(packageData.version)
+	.usage('<CityGML-file>|<directory with CityGML-files>')
 	.parse(process.argv)
 
 if (program.args < 1) {
 	program.help()
+}
+else {
+	cityNode.import(program.args[0])
 }
