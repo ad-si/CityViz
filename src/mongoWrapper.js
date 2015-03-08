6be2dcf08a81236c7777451b15d75c6e7b51d0c3
Adrian Sieber <mail@adriansieber.com>
2015-03-09 00:59:52 +0100

    Add skeleton code for mongodb connection

---
+++ src/mongoWrapper.js
@@ -0,0 +1,23 @@
+var mongodb = require('mongodb'),
+	mongoWrapper = {}
+
+
+mongoWrapper.createTable = function () {
+	// TODO
+	// Creates the buildings table
+}
+
+mongoWrapper.insert = function (data) {
+
+	// TODO
+	// Inserts data into buildings table
+}
+
+mongoWrapper.getAll = function(){
+
+	// TODO
+	// Returns promise which will eventually resolve
+	// to an array of buildings
+}
+
+module.exports = mongoWrapper
