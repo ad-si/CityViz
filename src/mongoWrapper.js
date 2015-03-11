b7fdad8c9a8046aa046bfc2a1d100c1965e613a9
stephan.detje <stephan.detje@student.hpi.uni-potsdam.de>
2015-03-11 23:06:37 +0100

    added promised-mongo and implemented mongoWrapper

--- src/mongoWrapper.js
+++ src/mongoWrapper.js
@@ -1,23 +1,30 @@
-var mongodb = require('mongodb'),
+var pmongo = require('promised-mongo'),
 	mongoWrapper = {}
 
 
+//not relevant, collections are created implicitly when data is inserted
 mongoWrapper.createTable = function () {
-	// TODO
-	// Creates the buildings table
+	return pmongo("mongodb://localhost:27017/cityViz")
+		.createCollection('buildings')
+		.catch(function (error) {
+			console.error(error.message)
+		})
+		.then(function (response) {
+			console.log(response)
+		})
 }
 
 mongoWrapper.insert = function (data) {
-
-	// TODO
-	// Inserts data into buildings table
+	return pmongo("mongodb://localhost:27017/cityViz")
+		.buildings
+		.insert(data)
 }
 
 mongoWrapper.getAll = function(){
-
-	// TODO
-	// Returns promise which will eventually resolve
-	// to an array of buildings
+	return pmongo("mongodb://localhost:27017/cityViz")
+		.buildings
+		.find()
+		.toArray()
 }
 
 module.exports = mongoWrapper
