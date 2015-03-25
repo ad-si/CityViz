a05b5068338ca9bdab9cfc85f5e7cff782b97292
Adrian Sieber <mail@adriansieber.com>
2015-03-25 17:56:29 +0100

    Enable rethinkdb import of directories

--- cli/cityviz-rdb-import.js
+++ cli/cityviz-rdb-import.js
@@ -15,6 +15,59 @@ var fs = require('fs'),
 	rdbWrapper = require('../src/rdbWrapper.js')
 
 
+function importFile(filePath, callback) {
+	citygml2gltf(
+		{inputFile: filePath},
+		function (error, cityObjects) {
+
+			if (error)
+				throw error
+
+			cityObjects = cityObjects.filter(function (item) {
+				return item != null
+			})
+
+			process.stdout.write(
+				'\n\nInsert ' +
+				cityObjects.length +
+				' cityObjects into database'
+			)
+
+			return rdbWrapper
+				.insert(cityObjects)
+				.then(function () {
+					console.log(' ✔')
+
+					if (callback && typeof callback === 'function')
+						callback()
+				})
+		}
+	)
+}
+
+function importLoop(files, index) {
+
+	if (files[index]) {
+
+		console.log(
+			'\n\nImport file ' + (index + 1) + ' of ' + files.length + ': ' +
+			files[index] + '\n'
+		)
+
+		importFile(files[index], function () {
+			importLoop(files, ++index)
+		})
+	}
+	else {
+		console.log('\n\nImported all files!\n')
+		console.timeEnd('Database import duration')
+	}
+}
+
+function isXmlFile(path) {
+	return /\.xml$/gi.test(path)
+}
+
 program.parse(process.argv)
 
 if (program.args.length < 1) {
@@ -27,25 +80,29 @@ if (String(program.args[0])[0] === '/')
 else
 	importPath = path.join(process.cwd(), program.args[0])
 
-rdbWrapper
-	.createTable()
-	.then(function () {
+console.time('Database import duration')
 
-		citygml2gltf(
-			{inputFile: importPath},
-			function (error, buildings) {
+fs.readdir(importPath, function (directoryError, filePaths) {
 
-				if (error)
-					throw error
+	rdbWrapper
+		.createTable()
+		.then(function () {
 
-				buildings.forEach(function (cityObject) {
-					rdbWrapper
-						.insert(cityObject)
-						.then(function () {
-							console.log(
-								'Imported cityobject ' + cityObject.gmlid
-							)
-						})
-				})
-			})
-	})
+			if (directoryError)
+				importFile(importPath)
+
+			else {
+
+				var xmlFiles = filePaths
+					.filter(isXmlFile)
+					.map(function (filePath) {
+						return path.join(importPath, filePath)
+					})
+
+				console.log('\nImport ' + xmlFiles.length + ' files')
+
+				importLoop(xmlFiles, 0)
+			}
+		})
+})
+
